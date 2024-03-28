774ced1f90c7fa1d1875625089ce242f74faa073
shakgill786 <sgillani1@gmail.com>
2024-03-28 19:54:29 -0400

    problem 3 resolved

--- aa02-pojo-basics/problems/03-set-key-in-object.js
+++ aa02-pojo-basics/problems/03-set-key-in-object.js
@@ -14,8 +14,14 @@ setKeyInObject(obj1, "num", 3); // => {str: "hello", num: 3}
 ***********************************************************************/
 
 function setKeyInObject(obj, string, value) {
-  // Your code here 
+  obj[string] = value; // Add a new key-value pair to the object using the provided string as key and value as value                      
+  return obj;          // Return the modified object
 }
 
+let obj = {};          //Create an empty object
+console.log(setKeyInObject(obj,"apple", "yum")); // Call the function setKeyInObject with the empty object, "apple", and "yum" as parameters and log the result
+
+let obj1 = {str: "hello"};  //Create an object with a key "str" and value "hello"
+console.log(setKeyInObject(obj1,"num",3));  // Call the function with the object obj1, "num" and 3 as paramenters
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = setKeyInObject;
