f815785ef7d5e71b9ddf571d121b907b17416d74
joshuamaxey <joshua.maxey.leija@icloud.com>
2024-03-28 19:25:20 -0500

    problem 4

--- aa02-pojo-basics/problems/04-get-full-name.js
+++ aa02-pojo-basics/problems/04-get-full-name.js
@@ -10,9 +10,17 @@ let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
 getFullName(p2); // => 'Charlie Brown'
 ***********************************************************************/
 
+
+
 function getFullName(person) {
-  // Your code here 
+  return person.firstName + ' ' + person.lastName;
 }
+
+
+let p1 = {firstName: 'John', lastName: 'Doe'};
+console.log(getFullName(p1)); // => 'John Doe'
+let p2 = {firstName: 'Charlie', lastName: 'Brown', age: 9};
+console.log(getFullName(p2)); // => 'Charlie Brown'
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = getFullName;
